98856b7c1f90a4f86324398c232ef4e19ad92788
floris.schmidt <floris.schmidt@hotmail.com>
2021-06-11 11:34:37 +0200

    fix

--- extract.py
+++ extract.py
@@ -26,15 +26,15 @@ def extract_city(i,high,low):
 
     high_city = high_city.rename({'Temperature sfc':'Temperature'},axis='columns')
     low_city = low_city.rename({'Temperature sfc':'Temperature'},axis='columns')
+
     if ('Temperature' not in high_city.columns):
         high_city = high_city.rename({'Temperature 2 m elevation corrected':'Temperature'},axis='columns')
         low_city = low_city.rename({'Temperature 2 m elevation corrected':'Temperature'},axis='columns')
     high_city.index.names = ['dt']
     low_city.index.names = ['dt']
+
     high_city = high_city[pd.notnull(high_city['Temperature'])]
     low_city = low_city[pd.notnull(low_city['Temperature'])]
-    high_city = high_city.drop_duplicates()
-    low_city = low_city.drop_duplicates()
 
     high_city.to_csv(Path('extracted/high/{}.csv'.format(cityRemap.remaped.get(city_name))))
     low_city.to_csv(Path('extracted/low/{}.csv'.format(cityRemap.remaped.get(city_name))))
@@ -48,9 +48,6 @@ def extract_city(i,high,low):
 #extract basic meteoblue
 high_bsl = pd.read_csv('Materials/MeteoBlue_basic-weather-variables-only/dataexport_20210403T181231_hi-res_bsl.csv', low_memory=False)
 low_bsl = pd.read_csv('Materials/MeteoBlue_basic-weather-variables-only/dataexport_20210403T181428_lo-res_bsl.csv', low_memory=False)
-
-high_bsl = high_bsl.drop_duplicates()
-low_bsl = low_bsl.drop_duplicates()
 
 high_bsl = high_bsl.set_index('location');
 low_bsl = low_bsl.set_index('location');
@@ -66,9 +63,6 @@ low = pd.read_csv("Materials/MeteoBlue_all-variables-incl-radiation/dataexport_2
 high = high.set_index('location');
 low = low.set_index('location');
 
-high = high.drop_duplicates()
-low = low.drop_duplicates()
-
 for i in range(10):
     extract_city(i,high,low)
 
@@ -82,9 +76,9 @@ all_csv_files = [file
 for file in all_csv_files:
     city = pd.read_csv(file)
     city.dt = city.dt.astype('datetime64[s]')
-    city = city.drop_duplicates()
     city = city.set_index('dt')
     city_name = city['city_name'][0]
     city = city.drop(columns=['lat','lon','weather_icon','dt_iso','timezone','city_name','weather_id','weather_main','weather_description'])
     city = city.rename({'temp':'Temperature'},axis='columns')
+    city = city.drop_duplicates(subset=['Temperature'])
     city.to_csv(Path('extracted/owm/{}.csv'.format(cityRemap.remaped.get(city_name))))
